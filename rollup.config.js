a3a2495f2c5a073abddbd19a1ef037c028114b5f
zhu315 <3150905455@qq.com>
2020-05-23 22:25:39 +0800

    videoRTP.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ import rollup from './rollup.base.config'
 export default rollup([
   { input: 'src/index.js', file: 'lib/index.js' },
   { input: 'src/index.js', file: 'lib/index.min.js', env: 'prod' },
+
   { input: 'src/index.js', file: 'videoRTP.min.js', name: 'videoRTP', format: 'umd', env: 'prod' },
+  { input: 'src/index.js', file: 'videoRTP.js', name: 'videoRTP', format: 'umd' },
 
   { input: 'src/pressVideo.js', file: 'lib/pressVideo.js' },
   { input: 'src/pressVideo.js', file: 'lib/pressVideo.min.js', env: 'prod' },
